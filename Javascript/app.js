80bb2c8048ce6158e1f155b229b116dbefaebd6e
Giancarlo Ferrucho <gferrucho@gmail.com>
2017-12-05 21:22:07 -0500

    Pushing all files to master on GitHub

---
+++ Javascript/app.js
@@ -0,0 +1,8 @@
+let opening="Hello there reader!";
+let message="If you're reading this you should be proud. You have discovered Uncle Sam LLC. and we want you!";
+let closing="Please call my assistent, Jenny, at the number listed below at your earliest convinience. Thank you!";
+const PHONE_NUMBER = "(305) 867-5309";
+console.log(opening);
+console.log(message);
+console.log(closing);
+console.log(PHONE_NUMBER);
